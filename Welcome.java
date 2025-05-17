3a31d6dca09bc6ff2bcb4ff68a51c545bcc13eef
balex_ug <bazigu.alex@gmail.com>
2025-05-17 10:59:55 +0300

    Add files via upload
    Initial version

---
+++ Welcome.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+alexis
+ */
+
+package vu.welcome;
+
+/**
+ *
+ * @author Admin
+ */
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome David");
+        System.out.println("It will be instructrd by Alex Bazigu");
+       
+        /* Declaring variables*/
+        int a =8;
+        int b =8;
+        //Computing the sum of a and b
+        int sum = a+b;
+        
+        System.out.println(sum);
+    }
+   
+}
